39fb8720811ed8f080aa73a867b05f63a9b6c411
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-06-19 13:57:58 +0300

    feat(chats): add chat preview component structure [WS-7](https://webitel.atlassian.net/browse/WS-7)
    - Introduced new components for chat preview: header, body, and footer.
    - Implemented a main chat-preview component to encapsulate these subcomponents.
    - Defined TypeScript types for task previews and chat previews to enhance type safety.
    - Updated the workspace layout to accommodate the new chat list structure.

---
+++ src/features/chats/components/chat-preview/chat-preview.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="chat-preview">
+        <router-link :to="`/chats/${threadId}`">
+            <chat-preview-header
+                :name="thread.subject ?? ''"
+                :username="thread.subject ?? ''"
+                :unread-count="unreadCount"
+                :avatar="avatar"
+            />
+            <chat-preview-body
+                :last-msg="thread.lastMsg"
+            />
+            <chat-preview-footer
+                :queue="task.queue"
+            />
+        </router-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { TaskPreview } from '../../types/ChatPreview.types';
+import ChatPreviewHeader from './header/chat-preview-header.vue';
+import { computed } from 'vue';
+
+const props = defineProps<{
+	task: TaskPreview;
+}>();
+
+const thread = computed(() => props.task.distribute.communication.thread);
+const threadId = computed(() => props.task.distribute.member_channel_id);
+const avatar = computed(() => '');
+const unreadCount = computed(() => 0);
+</script>
+
+<style scoped>
+</style>
--- src/ui/pages/modules/chats/components/the-chats-workspace.vue
+++ src/ui/pages/modules/chats/components/the-chats-workspace.vue
@@ -1,6 +1,11 @@
 <template>
     <section class="the-chats-workspace">
         <h1>Chats Workspace</h1>
+        <div class="chat-list">
+            <!-- <div class="chat-item" v-for="chat in chatList" :key="chat.id">
+                <h2>{{ chat.name }}</h2>
+            </div> -->
+        </div>
     </section>
 </template>
 
