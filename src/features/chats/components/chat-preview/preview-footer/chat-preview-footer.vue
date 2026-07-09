40ee23a753592a6d009da7f78107bd9e4c62ee2f
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-07-09 11:50:29 +0300

    refactor(chats): update chat preview components and types for improved structure and clarity [WS-7](https://webitel.atlassian.net/browse/WS-7)
    - Removed deprecated TaskPreview type and replaced it with Task type from webitel-sdk.
    - Introduced new components for chat preview header, body, and footer to enhance modularity.
    - Updated references in the chat previews list to align with the new structure.
    - Cleaned up unused properties and improved type definitions for better type safety.

--- src/features/chats/components/chat-preview/preview-footer/chat-preview-footer.vue
+++ src/features/chats/components/chat-preview/preview-footer/chat-preview-footer.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="chat-preview-footer">
-    <wt-chip
-      v-if="queue"
-    >
-      {{ queue }}
+    <wt-chip v-if="queue.name">
+      {{ queue.name }}
     </wt-chip>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script
+  lang="ts"
+  setup
+>
 import { WtChip } from '@webitel/ui-sdk/components';
 
 defineProps<{
-	queue: string; // string, or object with {id, name}
+	queue: {
+		id: string | number;
+		name: string;
+	}; // string, or object with {id, name}
 }>();
 </script>
 
